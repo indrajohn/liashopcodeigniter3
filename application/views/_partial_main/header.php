b71e2b5699e4b1084387dfa655b7d414d74008db
indrajohn <indrajohn2@gmail.com>
2021-04-07 17:29:40 +0700

    change to template styling

--- application/views/_partial_main/header.php
+++ application/views/_partial_main/header.php
@@ -5,22 +5,52 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <html class="no-js" lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Lia Shop</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
-    <link rel="apple-touch-icon" href="assets/apple-touch-icon.png">
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Lia Shop</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
+	<link rel="apple-touch-icon" href="assets/apple-touch-icon.png">
+	<link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap"
+		rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.css" type="text/css">
-    <link rel="stylesheet" href="assets/css/toolbar.css" type="text/css">
-    <link rel="stylesheet" href="assets/css/magnificpopup.css" type="text/css" />
-    <link rel="stylesheet" href="assets/css/font-awesome/fontawesome.min.css" type="text/css" />
-    <link rel="stylesheet" href="assets/css/elegant-icons.css" type="text/css">
-    <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.0/magnific-popup.min.css"
+		integrity="sha512-nIm/JGUwrzblLex/meoxJSPdAKQOe2bLhnrZ81g5Jbh519z8GFJIWu87WAhBH+RAyGbM4+U3S2h+kL5JoV6/wA=="
+		crossorigin="anonymous" />
+	<!--<link rel="stylesheet" href="assets/css/font-awesome/fontawesome.min.css" type="text/css" /> -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet"
+		crossorigin="anonymous" />
 
-    <!-- All css 
+	<!--<link rel="stylesheet" href="assets/css/toolbar.css" type="text/css">-->
+	<link rel="stylesheet" href="assets/css/elegant-icons.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+
+
+
+	<!-----------------------
+  	------	 Script  --------
+	----------------------- -->
+	<script src="assets/js/jquery-3.6.0.jquery.min.js">
+	</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.0/jquery.magnific-popup.min.js"
+		integrity="sha512-+m6t3R87+6LdtYiCzRhC5+E0l4VQ9qIT1H9+t1wmHkMJvvUQNI5MKKb7b08WL4Kgp9K0IBgHDSLCRJk05cFUYg=="
+		crossorigin="anonymous">
+	</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"
+		defer>
+	</script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+	</script>
+	<script src="assets/js/font-awesome/font-awesome-all.min.js" defer></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/mixitup/3.3.1/mixitup.min.js" defer></script>
+	<script src="assets/js/main.js" defer></script>
+
+	<!-- All css 
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
     <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">-->
--- application/views/_partial_main/navbar.php
+++ application/views/_partial_main/navbar.php
@@ -1,60 +1,52 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#"> <img src="assets/images/logo.png" /></a>
-        <button class="navbar-toggler navbar-toggler-icon" type="button" data-bs-toggle="offcanvas"
-            data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-            aria-label="Toggle navigation">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="#"> <img src="assets/images/logo.png" /></a>
+		<button class="navbar-toggler navbar-toggler-icon" type="button" data-bs-toggle="offcanvas"
+			data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+			aria-label="Toggle navigation">
 
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto toolbar-link">
-                <li class="nav-item" style="cursor:default">
-                    <a class="nav-link <?php if ($head == 'home') echo 'active' ?>" href="<?php echo base_url() ?>">
-                        Home </i> </a>
-                </li>
-                <li class="nav-item ms-5" style="cursor:default">
-                    <a class="nav-link <?php if ($head == 'aboutus') echo 'active' ?>" href="about-us"> About Us</i>
-                    </a>
-                </li>
-                <li class="nav-item ms-5" style="cursor:default">
-                    <a class="nav-link <?php if ($head == 'product') echo 'active' ?>" href="product"> Product</i> </a>
-                </li>
-                <li class="nav-item ms-5" style="cursor:default">
-                    <a class="nav-link <?php if ($head == 'contactus') echo 'active' ?>" href="contact-us">Contact
-                        us</i> </a>
-                </li>
-            </ul>
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mx-auto toolbar-link">
+				<li class="nav-item" style="cursor:default">
+					<a class="nav-link <?php if ($head == 'home') echo 'active' ?>" href="<?php echo base_url() ?>">
+						Home
+					</a>
+				</li>
+				<li class="nav-item ms-5" style="cursor:default">
+					<a class="nav-link <?php if ($head == 'aboutus') echo 'active' ?>" href="about-us">
+						About Us
+					</a>
+				</li>
+				<li class="nav-item ms-5" style="cursor:default">
+					<a class="nav-link <?php if ($head == 'product') echo 'active' ?>" href="product">
+						Product
+					</a>
+				</li>
+				<li class="nav-item ms-5" style="cursor:default">
+					<a class="nav-link <?php if ($head == 'contactus') echo 'active' ?>" href="contact-us">
+						Contact us
+					</a>
+				</li>
+			</ul>
+			<ul class="navbar-nav ml-auto">
+				<!-- Authentication Links -->
 
-                <li class="nav-item">
-                    <a class="nav-link" href="#"> <i class="fas fa-search"></i> </a>
-                </li>
-                <li class="nav-item" style="cursor:default">
-                    <div class="nav-link" href=""> | </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="fas fa-user"></i>
-                    </a>
+				<li class="nav-item">
+					<a class="nav-link" href="#"> <i class="fas fa-search"></i> </a>
+				</li>
+				<li class="nav-item" style="cursor:default">
+					<div class="nav-link" href=""> | </div>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+						data-bs-toggle="dropdown" aria-expanded="false">
+						<i class="fas fa-user"></i>
+					</a>
 
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <?php
-                        if ($this->session->userdata('username') == null) {
-                            echo "
-                <li><a class='dropdown-item' href='login'>Login</a></li>
-                <li><a class='dropdown-item' href='register'>Register</a></li>";
-                        } else {
-                            echo "
-                <li><a class='dropdown-item' href='profile'>Profile</a></li>
-                <li><a class='dropdown-item' href='changepass'>Change Password</a></li>
-                <li><a class='dropdown-item' href='logout'>Log Out</a></li>";
-                        }
-                        ?>
-                    </ul>
-                </li>
-                <?php
+
+				</li>
+				<?php
                 if ($this->session->userdata('username') != null) {
                     echo "<li class='nav-item' style='cursor:default'>
                       		  <div class='nav-link' href=''> | </div>
@@ -66,31 +58,31 @@
                 }
                 ?>
 
-            </ul>
-        </div>
-        <div class="offcanvas offcanvas-start" tabindex="-1" id="navbarNavAltMarkup"
-            aria-labelledby="navbarNavAltMarkup">
-            <div class="navbar-nav mr-auto toolbar-link">
-                <div class="offcanvas-close" data-bs-toggle="offcanvas" data-bs-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">+</div>
-                <div class="offcanvas-content ms-2">
-                    <li class="nav-item mb-4" style="cursor:default">
-                        <a class="navbar-brand" href="#"> <img src="assets/images/logo.png" /></a>
-                    </li>
-                    <li class="nav-item" style="cursor:default">
-                        <a class="nav-link active" aria-current="page" href="<?php echo base_url(); ?>">Home</a>
-                    </li>
-                    <li class="nav-item" style="cursor:default">
-                        <a class="nav-link" href="about-us">About Us</a>
-                    </li>
-                    <li class="nav-item" style="cursor:default">
-                        <a class="nav-link" href="product">Product</a>
-                    </li>
-                    <li class="nav-item" style="cursor:default">
-                        <a class="nav-link" href="contact-us">Contact Us</a>
-                    </li>
-                </div>
-            </div>
-        </div>
-    </div>
+			</ul>
+		</div>
+		<div class="offcanvas offcanvas-start" tabindex="-1" id="navbarNavAltMarkup"
+			aria-labelledby="navbarNavAltMarkup">
+			<div class="navbar-nav mr-auto toolbar-link">
+				<div class="offcanvas-close" data-bs-toggle="offcanvas" data-bs-target="#navbarNavAltMarkup"
+					aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">+</div>
+				<div class="offcanvas-content ms-2">
+					<li class="nav-item mb-4" style="cursor:default">
+						<a class="navbar-brand" href="#"> <img src="assets/images/logo.png" /></a>
+					</li>
+					<li class="nav-item" style="cursor:default">
+						<a class="nav-link active" aria-current="page" href="<?php echo base_url(); ?>">Home</a>
+					</li>
+					<li class="nav-item" style="cursor:default">
+						<a class="nav-link" href="about-us">About Us</a>
+					</li>
+					<li class="nav-item" style="cursor:default">
+						<a class="nav-link" href="product">Product</a>
+					</li>
+					<li class="nav-item" style="cursor:default">
+						<a class="nav-link" href="contact-us">Contact Us</a>
+					</li>
+				</div>
+			</div>
+		</div>
+	</div>
 </nav>
